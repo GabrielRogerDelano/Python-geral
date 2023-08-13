72e02381145556e3dbedb2617d332c6449526869
Gabriel Roger <122801172+GabrielRogerDelano@users.noreply.github.com>
2023-08-13 12:55:54 -0300

    adicionando a funcao de sacar

--- cursoEmVideo/Conta.py
+++ cursoEmVideo/Conta.py
@@ -14,15 +14,23 @@ class ContaBancaria:
             print("Valor inválido. O depósito deve ser maior que zero.")
             return False
 
+    def sacar(self, valor):
+        if (valor < self.saldo) and (valor > 0):
+            self.saldo -= valor
+            self.historico.append({"data": datetime.now(), "valor": -valor, "tipo": "Saque"})
+            return True
+        else:
+            print("Valor inválido. O saque nao pode ser maior do que a conta nem menor que zero")
+
     def extrato(self):
-        print("Extrato:")
+        print("\033[0;33mExtrato:")
         print(f"Saldo atual: R$ {self.saldo:.2f}")
         print("Histórico de transações:")
         for transacao in self.historico:
             data = transacao["data"].strftime("%d/%m/%Y %H:%M:%S")
             valor = transacao["valor"]
             tipo = transacao["tipo"]
-            print(f"{data} - {tipo}: R$ {valor:.2f}")
+            print(f"{data} - {tipo}: R$ {valor:.2f}\033[m")
 
 
 def main():
@@ -30,19 +38,28 @@ def main():
 
     while True:
         print("\n1. Fazer depósito")
-        print("2. Mostrar extrato")
-        print("3. Sair")
-        opcao = int(input("Escolha uma opção (1, 2 ou 3): "))
+        print("2. Fazer saque")
+        print("3. Mostrar extrato")
+        print("4. Sair")
+        opcao = int(input("\033[1;36mEscolha uma opção (1, 2, 3 ou 4): \033[m"))
 
         if opcao == 1:
             valor = float(input("Digite o valor do depósito: "))
             if conta.depositar(valor):
-                print("Depósito realizado com sucesso!")
+                print("\033[0;32mDepósito realizado com sucesso!\033[m")
+
         elif opcao == 2:
-            conta.extrato()
+            valor = float(input("Digite o valor do saque: "))
+            if conta.sacar(valor):
+                print("\033[0;32mSaque realizado com sucesso!\033[m")
+
         elif opcao == 3:
+            conta.extrato()
+
+        elif opcao == 4:
             print("Encerrando o programa.")
             break
+
         else:
             print("Opção inválida. Escolha uma opção válida.")
 
